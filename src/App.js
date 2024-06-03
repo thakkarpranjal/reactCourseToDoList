8756ae53d5208a604358b2bd5b7ff476e024ad32
unknown <pranjal.thakkar@blobstation.com>
2024-06-03 19:06:23 +0530

    React course To do list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To do List</h1>
+      <div className="todo-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addTask}>+</button>
+      </div>
+
+      <div className="todo-list-container">
+        {tasks.map((task, index) => (
+          <div key={index} className="todo-list">
+            <p>{task}</p>
+            <div className="actions">
+              <input type="checkbox" />
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
